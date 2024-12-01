02a860abb15937c00fad1a745296a982cabc8e65
toxictoxic321 <148807635+toxictoxic321@users.noreply.github.com>
2024-12-01 11:08:51 +0500

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,24 @@
+document.getElementById('edit-profile').addEventListener('click', () => {
+    console.log('Edit Profile button clicked');
+  });
+  
+  document.getElementById('browse-fields').addEventListener('click', () => {
+    console.log('Browse Fields button clicked');
+  });
+  
+  document.getElementById('add-favorite').addEventListener('click', () => {
+    console.log('Add Favourite Course button clicked');
+  });
+  
+  document.getElementById('provide-feedback').addEventListener('click', () => {
+    console.log('Provide Feedback button clicked');
+  });
+  
+  document.getElementById('send-inquiry').addEventListener('click', () => {
+    console.log('Send Inquiry button clicked');
+  });
+  
+  document.getElementById('logout').addEventListener('click', () => {
+    console.log('Log Out button clicked');
+  });
+  
