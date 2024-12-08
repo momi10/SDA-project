79e13272f335a09ae685f2f8dbdc5f6959a53667
Mohaiman Ahmad <163326435+momi10@users.noreply.github.com>
2024-12-09 00:21:39 +0500

    Update feedbacll.js

--- feedbacll.js
+++ feedbacll.js
@@ -1,1 +1,7 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+  const stars = document.querySelectorAll(".star");
+  const resetButton = document.getElementById("reset-button");
+  const submitButton = document.getElementById("submit-button");
+  const feedbackText = document.getElementById("feedback");
+  let selectedRating = 0;
